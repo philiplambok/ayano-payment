6158aeaab7b3bff0a558cbe22bb068a8122c5c5f
Philip lambok <philiplambok71@gmail.com>
2018-07-28 11:32:14 +0700

    add create, update, destroy role

--- app/controllers/api/roles_controller.rb
+++ app/controllers/api/roles_controller.rb
@@ -1,6 +1,6 @@
 class Api::RolesController < ApplicationController
-  before_action :set_role, only: [:show]
-  before_action :validate_avaiability, only: [:show]
+  before_action :set_role, only: [:show, :update, :destroy]
+  before_action :validate_avaiability, only: [:show, :update, :destroy]
   before_action :get_all_roles, only: [:index] 
     
   def index 
@@ -9,6 +9,29 @@ class Api::RolesController < ApplicationController
 
   def show
     json_response(@role) 
+  end
+
+  def create
+    @role = Role.new role_params
+
+    if @role.save
+      json_response(@role)
+    else 
+      error_response(code: "422", message: "Name can't be blank") if @role.errors[:name].include?("can't be blank")
+    end
+  end
+
+  def update
+    if @role.update_attributes role_params 
+      json_response(@role)
+    end
+  end
+
+  def destroy 
+    cache = @role
+    if @role.destroy 
+      json_response(cache)
+    end
   end
 
   private 
@@ -22,5 +45,9 @@ class Api::RolesController < ApplicationController
 
   def get_all_roles 
     @roles = Role.all
+  end
+
+  def role_params 
+    params.require(:role).permit(:name)
   end
 end
--- spec/requests/api/api_roles_spec.rb
+++ spec/requests/api/api_roles_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Roles", type: :request do
-  describe "/api/roles/:id" do
+  describe "show role" do
     it "return role if role found" do 
       role = create(:role, name: "admin")
       get api_role_path(role)
@@ -16,13 +16,63 @@ RSpec.describe "Api::Roles", type: :request do
     end
   end
 
-  describe "/api/roles" do 
+  describe "show all roles" do 
     it "returns roles" do
       admin_role = create(:role, name: "admin")
       member_role = create(:role, name: "member")
       get api_roles_path 
-      
+
       expect(response.body).to include(admin_role.id.to_s, admin_role.name, member_role.id.to_s, admin_role.name)      
+    end
+  end
+
+  describe "create new role" do
+    it "return created role" do
+      role = build(:role, name: "admin")
+      post api_roles_path, params: { role: { name: role.name } }
+
+      expect(response.body).to include(role.name)
+    end
+
+    it "return name validation error" do 
+      role = build(:role, name: nil)
+      post api_roles_path, params: { role: { name: role.name } }
+
+      expect(response.body).to include("422", "Name can't be blank")
+    end
+  end
+
+  describe "update role" do 
+    it "return updated role" do 
+      role = create(:role, name: "member")
+      updated_role_name = "premium"
+
+      put api_role_path(role), params: { role: { name: updated_role_name } }
+
+      expect(response.body).to include(role.id.to_s, updated_role_name)
+    end
+
+    it "return not found message if role not exist" do 
+      updated_role_name = "premium"
+      put "/api/roles/99", params: { role: { name: updated_role_name } }
+
+      expect(response.body).to include("404", "Sorry, role not found")
+    end
+  end
+
+  describe "delete role" do
+    it "return deleted role" do 
+      role = create(:role)
+
+      delete api_role_path(role)
+
+      expect(response.body).to include("#{role.id}", role.name)
+    end
+
+    it "return not found message if role not exists" do 
+      delete "/api/roles/99"
+
+      expect(response.body).to include("404", "Sorry, role not found")
     end
   end
 end
