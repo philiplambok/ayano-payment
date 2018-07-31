1a8a3a4ad4842e718fd10d8cfc60fbb1270e7b0f
Philip lambok <philiplambok71@gmail.com>
2018-07-31 12:31:20 +0700

    add validation to logs controller

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -1,12 +1,27 @@
 class Api::LogsController < ApplicationController
   before_action :set_user 
+  before_action :validate_avaiability
+  before_action :validate_auth
+  before_action :validate_permission
 
   def index
     json_response(@user.logs)
   end
 
   private 
-  def set_user 
+  def set_user
     @user = User.find_by_id(params[:id])
+  end
+
+  def validate_avaiability
+    error_response(template: :not_found, model: "user") unless @user 
+  end
+
+  def validate_auth 
+    error_response(template: :unauthorized) unless current_user
+  end
+
+  def validate_permission
+    error_response(template: :forbidden) unless current_user.owner_or_admin?(@user)
   end
 end
--- spec/requests/api/logs/show_logs_spec.rb
+++ spec/requests/api/logs/show_logs_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe "show logs", type: :request do
     user.open_deposit!
   end
 
-  describe "take deposit" do
+  context "as owner" do 
     before do 
       user.add_deposit({ amount: "100000" })
     end
 
-    context "as owner" do
+    describe "take deposit" do
       it "return take deposit log message" do 
         request_post_deposit(as_user: user, user: user, type: "take", amount: "50000")
         expect(response.body).to include("amount", "50000")
@@ -22,10 +22,8 @@ RSpec.describe "show logs", type: :request do
         expect(response.body).to include("message", "You take deposit 50000")
       end
     end
-  end
 
-  describe "save deposit" do 
-    context "as owner" do 
+    describe "save deposit" do 
       it "return added deposit message" do 
         request_post_deposit(as_user: user, user: user, type: "save", amount: "50000")
         expect(response.body).to include("amount", "50000")
@@ -34,15 +32,13 @@ RSpec.describe "show logs", type: :request do
         expect(response.body).to include("message", "You added deposit 50000")
       end
     end
-  end
 
-  describe "transfer" do 
-    before do 
-      target_user.open_deposit!
-      user.add_deposit({ amount: "100000" })
-    end
+    describe "transfer" do 
+      before do 
+        target_user.open_deposit!
+        user.add_deposit({ amount: "100000" })
+      end
 
-    context "as owner" do 
       it "return send deposit messsage" do 
         request_transaction({ user: user, to: target_user, amount: "20000", as: user })
 
@@ -51,5 +47,30 @@ RSpec.describe "show logs", type: :request do
         expect(response.body).to include("message", "You send 20000 to #{target_user.username}")
       end
     end
+  end
+
+  context "as hacker" do 
+    let (:hacker) { create(:hacker) }
+    it "return forbidden message" do 
+      get logs_api_user_path(user), headers: auth_params(hacker)
+
+      expect_forbidden
+    end
+  end
+
+  context "without auth" do 
+    it 'return not authenticated message' do
+      get logs_api_user_path(user), headers: nil
+
+      expect_not_authenticated
+    end
+  end
+
+  context "user didn't exist" do
+    it "return not found message" do 
+      get '/api/users/99/logs', headers: auth_params(user)
+
+      expect_not_found('user')
+    end 
   end
 end
